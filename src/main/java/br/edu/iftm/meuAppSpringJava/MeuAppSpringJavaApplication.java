7a5d7cf9d8436dcc1124a96c072ce4dd5395ea0f
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2025-08-19 16:16:08 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/iftm/meuAppSpringJava/MeuAppSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.meuAppSpringJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuAppSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuAppSpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/meuAppSpringJava/MeuAppSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.meuAppSpringJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuAppSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
